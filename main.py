a4f66e922f6f5d0f0c66fa423650b5db5fb3f377
Sirui Li <slray@nyu.edu>
2023-09-17 01:48:32 -0400

    Added end point

--- main.py
+++ main.py
@@ -2,49 +2,71 @@
 # Date: 9/17/23
 # Description:
 
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, Flask
+
+from question.ask import ask
+from util.transcript import get_transcript_detail, get_context_by_ts, get_summarized_transcript
+
+app = Flask(__name__)
 
 
-@questions.route('/questions', methods = ['POST'])
+@app.route('/', methods=['GET'])
+def hello():
+    return "hello"
+
+
+@app.route('/questions', methods=['POST'])
 def post_question():
     data = request.get_json()
 
     if not data:
         return jsonify({
             "Error": "API Invalid Input"
-        }), HTTP_400_BAD_REQUEST
+        })
 
     transcript_id = data.get('transcript_id')
     time_stamp = data.get('time_stamp')
     history_data = data.get('history_data')
     question_text = data.get('question_text')
 
-    exist = len(Transcripts.query.filter(Transcripts.id == transcript_id).all()) > 0
-    if not exist:
-        load_the_transcript(transcript_id)
+    # exist = len(Transcripts.query.filter(Transcripts.id == transcript_id).all()) > 0
+    # if not exist:
+    #     load_the_transcript(transcript_id)
+    #
+    # results = TranscriptDetails.query.filter(and_(TranscriptDetails.transcript_id == transcript_id,
+    #                                               and_(TranscriptDetails.start > time_stamp - int(INTERVAL_RANGE),
+    #                                                    TranscriptDetails.end < time_stamp + int(INTERVAL_RANGE)))).all()
+    # context = "".join([transcript.text for transcript in results]).replace('\n', " ")
+    # print(context)
+    raw_trans = get_transcript_detail(video_id=transcript_id)
 
-    results = TranscriptDetails.query.filter(and_(TranscriptDetails.transcript_id == transcript_id,and_(TranscriptDetails.start > time_stamp - int(INTERVAL_RANGE), TranscriptDetails.end < time_stamp + int(INTERVAL_RANGE)))).all()
-    context = "".join([transcript.text for transcript in results]).replace('\n', " ")
-    print(context)
-
+    context = get_context_by_ts(ts=int(time_stamp),
+                                raw_trans=raw_trans,
+                                interval=600)
     return jsonify({
         "answer": ask(context, question_text)
-    }), HTTP_200_OK
+    })
 
-@questions.route('/get_summary', methods = ['POST'])
+
+@app.route('/get_summary', methods=['POST'])
 def post_summary():
     data = request.get_json()
 
     if not data:
         return jsonify({
             "Error": "API Invalid Input"
-        }), HTTP_400_BAD_REQUEST
+        })
 
     video_id = data.get('video_id')
 
-    exist = Transcripts.query.filter(Transcripts.id == video_id).first()
-    print(exist)
+    # exist = Transcripts.query.filter(Transcripts.id == video_id).first()
+    # print(exist)
+    summ = get_summarized_transcript(video_id=video_id)
 
     return jsonify({
-        "answer": exist.summary
-    }), HTTP_200_OK
+        "answer": summ
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
